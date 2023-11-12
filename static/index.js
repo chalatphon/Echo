daa286f5a06ccd230c0fe772a7d01f8efa6d8845
Thanakrit66070262 <141338370+Thanakrit66070262@users.noreply.github.com>
2023-11-12 16:34:25 +0700

    echodbk

--- static/index.js
+++ static/index.js
@@ -1,10 +1,26 @@
 
+function AddData(){
+  var _event = document.getElementById("event").value;
+  var _routine = document.getElementById("routine").value;
+  var _date = document.getElementById("date").value;
+  const var_date = new Date(_date)
+  var calendarevent = {
+    "id": "wad7r4w3",
+    "name": "dwdwdiuj2",
+    "date": "October/17/2023",
+    "type": "holiday",
+  }
+  globalThis (calendarevent)
+  var jsoncalendar_event = JSON.stringify(calendarevent);
+  globalThis (jsoncalendar_event)
+  console.log(typeof jsoncalendar_event);
+
+}
 $("#calendar").evoCalendar({
-    theme:"Orange Coral",
     calendarEvents: [
   {
-    id: 'bHay68s', // Event's ID (required)
-    name: "Sunsu", // Event name (required)
+    id: "fewf28few", // Event's ID (required)
+    name: "Kinh", // Event name (required)
     date: "October/25/2023", // Event date (required)
     type: "holiday", // Event type (required)
     everyYear: true // Same event every year (optional)
@@ -16,36 +32,6 @@ $("#calendar").evoCalendar({
     type: "holiday", // Event type (required)
     everyYear: true // Same event every year (optional)
   },
-  {
-    id: 'bHay68s',
-    badge: "02/13 - 02/15", // Event badge (optional)
-    date: ["February/13/2020", "February/15/2020"], // Date range
-    description: "Vacation leave for 3 days.", // Event description (optional)
-    type: "event",
-    color: "#63d867" // Event custom color (optional)
-  }
-    ],
+  jsoncalendar_event
+  ]
 });
-function AddData(){
-    var _event = document.getElementById("event").value;
-    var _routine = document.getElementById("routine").value;
-    var _date = document.getElementById("date").value;
-    const var_date = new Date(_date)
-    console.log(var_date);
-    // var datalist;
-    // if (localStorage.getItem("datalist") == null) {
-    //     datalist = [];
-    // } else {
-    //     datalist = JSON.parse(localStorage.getItem("datalist")) 
-    // }
-    // datalist.push({
-    //     _event: _event,
-    //     _routine: _routine,
-    // });
-
-    // localStorage.setItem("datalist", JSON.stringify
-    // (datalist));
-    // showData();
-    // document.getElementById("_event").value = "";
-    // document.getElementById("_routine").value = "";
-}
